a002e4e7bd8cb5866d22853383fb0b8dcbc0b019
Leandro Acquati <leandroacquati@gmail.com>
2019-10-17 18:16:13 -0300

    change to click event

--- js/script.js
+++ js/script.js
@@ -4,26 +4,51 @@ var context = canvas.getContext('2d');
 var video = document.createElement("video");
 var videoIsPlaying = false;
 
-canvas.width = 1024;
-canvas.height = 768;
+// if (window.innerWidth < 720) {
+// 	canvas.width = window.innerWidth;
+// 	canvas.height = window.innerHeight;
+// } else {
+// 	canvas.width = 720;
+// 	canvas.height = 480;
+// }
+
+canvas.width = 720;
+canvas.height = 480;
 
 video.src = mediaSource;
 
-document.body.onkeyup = function (event) {
-	if (event.keyCode == 32) {
-		if (videoIsPlaying) {
-			videoIsPlaying = false;
-			video.pause();
-		} else {
-			videoIsPlaying = true;
-			video.play();
+// document.body.onkeyup = function (event) {
+// 	if (event.keyCode == 32) {
+// 		if (videoIsPlaying) {
+// 			videoIsPlaying = false;
+// 			video.pause();
+// 		} else {
+// 			videoIsPlaying = true;
+// 			video.play();
 
-			(function loop() {
-				if (!video.paused && !video.ended) {
-					context.drawImage(video, 0, 0);
-					setTimeout(loop, 1000 / 20);
-				}
-			})();
-		}
+// 			(function loop() {
+// 				if (!video.paused && !video.ended) {
+// 					context.drawImage(video, 0, 0);
+// 					setTimeout(loop, 1000 / 20);
+// 				}
+// 			})();
+// 		}
+// 	}
+// }
+
+window.onclick = function () {
+	if (videoIsPlaying) {
+		videoIsPlaying = false;
+		video.pause();
+	} else {
+		videoIsPlaying = true;
+		video.play();
+
+		(function loop() {
+			if (!video.paused && !video.ended) {
+				context.drawImage(video, 0, 0);
+				setTimeout(loop, 1000 / 20);
+			}
+		})();
 	}
 }
