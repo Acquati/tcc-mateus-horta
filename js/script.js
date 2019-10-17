30234175dd95de29bda5618db86e1fe1f23ade1a
Leandro Acquati <leandroacquati@gmail.com>
2019-10-17 17:38:28 -0300

    play pause working

--- js/script.js
+++ js/script.js
@@ -1,24 +1,31 @@
-var mediaSource = "http://upload.wikimedia.org/wikipedia/commons/7/79/Big_Buck_Bunny_small.ogv"
+var mediaSource = "./media/video-teste.mp4"
 var canvas = document.getElementById('Canvas');
 var context = canvas.getContext('2d');
 var video = document.createElement("video");
+var videoIsPlaying = false;
 
 canvas.width = 1024;
 canvas.height = 768;
 
 video.src = mediaSource;
-video.width = "100";
-video.height = "100";
+// video.width = 100;
+// video.height = 100;
 
 document.body.onkeyup = function (event) {
 	if (event.keyCode == 32) {
-		video.play();
-
-		(function loop() {
-			if (!video.paused && !video.ended) {
-				context.drawImage(video, 0, 0);
-				setTimeout(loop, 1000 / 30); // drawing at 30fps
-			}
-		})();
+		if (videoIsPlaying) {
+			videoIsPlaying = false;
+			video.pause();
+		} else {
+			videoIsPlaying = true;
+			video.play();
+	
+			(function loop() {
+				if (!video.paused && !video.ended) {
+					context.drawImage(video, 0, 0);
+					setTimeout(loop, 1000 / 20);
+				}
+			})();
+		}
 	}
 }
