d4c66274f30ad1820afd31dd287662c0f18edc16
Leandro Acquati <leandroacquati@gmail.com>
2019-10-29 18:15:52 -0300

    mudança de vídeo

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-var mediaSource = "https://res.cloudinary.com/acquati/video/upload/v1571345545/tcc-mateus-horta/video-teste_zeaf60.mp4"
-// var mediaSource = "./media/TCC_MATEUS_CORTETESTE.mp4"
+// var mediaSource = "https://res.cloudinary.com/acquati/video/upload/v1571345545/tcc-mateus-horta/video-teste_zeaf60.mp4"
+var mediaSource = "./media/TQFQFNMF_corte_3_25102019.mp4"
 // var mediaSource = "http://techslides.com/demos/sample-videos/small.mp4"
 var video = document.createElement("video");
 var videoIsPlaying = false;
@@ -10,14 +10,14 @@ var container = document.getElementById("container");
 
 var canvas = document.getElementById('Canvas');
 var context = canvas.getContext('2d');
-canvas.width = 720; // 560 1024
-canvas.height = 480; // 320 768
+canvas.width = 1024; // 560 480 1024
+canvas.height = 768; // 320 720 768
 
 var opaqueCanvas = document.createElement("canvas");
 opaqueCanvas.classList.add("opaque-canvas");
 var opaqueCanvasContext = opaqueCanvas.getContext('2d');
-opaqueCanvas.width = 720;
-opaqueCanvas.height = 480;
+opaqueCanvas.width = 1024;
+opaqueCanvas.height = 768;
 opaqueCanvasContext.globalAlpha = 0.01;
 
 function keyHandler() {
